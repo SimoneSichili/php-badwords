8b215228c141108f57623750a58e17f717a1d3f2
Simone <simone.sichili@gmail.com>
2021-01-19 15:50:48 +0100

    Add text length

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $text = "Il pene è l'organo sessuale maschile. I bambini, gli uomini e gli animali di sesso maschile, tutti hanno il pene. I bambini e i ragazzi spesso ridacchiano alla parola 'pene', ma è solo una parte del corpo, come dire 'braccio' o 'gamba'. Beh, diciamo che il pene è solo un po' più particolare, poiché è l'organo che gli uomini usano per il sesso e per la pipì. Alcuni uomini hanno molto a cuore questa parte del loro corpo, e ne provano orgoglio o vergogna, a seconda di alcuni parametri che ritengono fondamentali, quali la lunghezza e il diametro.";
+    $text = "Il pene è l'organo sessuale maschile. I bambini, gli uomini e gli animali di sesso maschile, tutti hanno il pene. I bambini e i ragazzi spesso ridacchiano alla parola 'pene', ma è solo una parte del corpo, come dire 'braccio' o 'gamba'. Beh, diciamo che il pene è solo un po' più particolare, poiché è l'organo che gli uomini usano per il sesso e per la pipì. Alcuni uomini hanno molto a cuore questa parte del loro corpo, e ne provano orgoglio o vergogna, a seconda di alcuni parametri del loro pene che ritengono fondamentali, quali la lunghezza e il diametro.";
 
     $badword = $_GET["word"];
 
@@ -14,6 +14,7 @@
 </head>
 <body>
     <h2>Testo originale</h2>
+    <h3>Lunghezza: <?php echo strlen($text) ?></h3>
     <p><?php echo $text ?></p>
     <h2>Testo censurato</h2>
     <p><?php echo str_replace("pene", $badword, $text) ?></p>
